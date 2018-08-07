e5cf536ee95bd65ada0de02599b0e78f61794134
wkwksama <mailpakgasing@gmail.com>
2018-08-07 23:02:07 +0700

    chat work

--- index.js
+++ index.js
@@ -7,14 +7,28 @@ const config = require('./bottender.config.js').line;
 const bot = new LineBot({
   accessToken: config.accessToken,
   channelSecret: config.channelSecret,
-  sessionStore: new MongoSessionStore('mongodb://localhost:27017/'),
+  sessionStore: new MongoSessionStore('mongodb://localhost:27017/lineBot'),
 });
 
 bot.onEvent(async context => {
-  await context.sendText('Hello World');
+  if (context.event.isFollow) {
+    await context.sendText('Hello, welcome to this bot!');
+  } else if (context.event.isText && context.event.text === 'How are you?') {
+    await context.sendText('I am fine.');
+  } else {
+    await context.sendText('I do not understand.');
+  }
+  context.replyImage(
+    'https://developers.line.me/media/messaging-api/messages/image-full-04fbba55.png',
+    'https://developers.line.me/media/messaging-api/messages/image-full-04fbba55.png'
+  );
 });
 
 const server = createServer(bot);
+
+server.get('/', (req, res) => {
+  res.json({hello: 'hello world'})
+})
 
 server.listen(5000, () => {
   console.log('server is running on 5000 port...');
